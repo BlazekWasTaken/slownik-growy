c2d3503fa9148ae7cf016a85958fc4cf44cdd1c1
Błażej Dymek <dymekblazej@outlook.com>
2023-08-09 21:15:15 +0200

    Created basic file read and program logic

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿List<string> words = new();
+
+bool isFilePathCorrect = true;
+while (isFilePathCorrect)
+{
+    Console.WriteLine("Gdzie jest plik ze słowami?");
+    try
+    {
+        string path = Console.ReadLine() ?? string.Empty;
+        words = File.ReadAllLines(path).ToList();
+        isFilePathCorrect = false;
+    }
+    catch (Exception e)
+    {
+        isFilePathCorrect = true;
+    }
+    Console.Clear();
+}
+
+bool isRunning = true;
+while (isRunning)
+{
+    Console.Write("Jakie słowo sprawdzić?: ");
+    try
+    {
+        string word = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine(words.Contains(word.ToLower()) ? "Słowo jest dobre :)" : "Słowo nie jest dobre :(");
+    }
+    catch
+    {
+        Console.WriteLine("Słowo nie jest dobre :(");
+    }
+    Console.ReadLine();
+    Console.Clear();
+}
