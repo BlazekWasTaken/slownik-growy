a9cd8dc89a403b49b3ebcd605975d9878f2defb2
Błażej Dymek <dymekblazej@outlook.com>
2023-08-10 21:19:22 +0200

    Added unzipping the file and reading the contents to a list

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 string tempPath = Path.GetTempPath();
 string zipPath = Path.Combine(tempPath, "slowa.zip");
+string filePath = Path.Combine(tempPath, "slowa.txt");
 
 string resourcePath = Assembly.GetExecutingAssembly().GetManifestResourceNames().FirstOrDefault(x => x.Contains("slowa.zip")) ?? string.Empty;
 var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcePath);
@@ -11,20 +12,24 @@ stream.CopyTo(fileStream);
 stream.Close();
 fileStream.Close();
 
-// while (true)
-// {
-//     Console.Write("Jakie słowo sprawdzić?: ");
-//     try
-//     {
-//         string word = Console.ReadLine() ?? string.Empty;
-//         Console.WriteLine(words.Contains(word.ToLower()) ? "Słowo jest dobre :)" : "Słowo nie jest dobre :(");
-//     }
-//     catch
-//     {
-//         Console.WriteLine("Słowo nie jest dobre :(");
-//     }
-//     Console.ReadLine();
-//     Console.Clear();
-// }
-
+System.IO.Compression.ZipFile.ExtractToDirectory(zipPath,tempPath);
 File.Delete(zipPath);
+
+List<string> words = File.ReadAllLines(filePath).ToList();
+File.Delete(filePath);
+
+while (true)
+{
+    Console.Write("Jakie słowo sprawdzić?: ");
+    try
+    {
+        string word = Console.ReadLine() ?? string.Empty;
+        Console.WriteLine(words.Contains(word.ToLower()) ? "Słowo jest dobre :)" : "Słowo nie jest dobre :(");
+    }
+    catch
+    {
+        Console.WriteLine("Słowo nie jest dobre :(");
+    }
+    Console.ReadLine();
+    Console.Clear();
+}
